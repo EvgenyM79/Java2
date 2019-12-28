b733732830186df9e8b4502411b85d47bdad2453
EvgenyM79 <56968324+EvgenyM79@users.noreply.github.com>
2019-12-28 23:10:42 +0500

    Add files via upload

---
+++ CalcUtil.java
@@ -0,0 +1,57 @@
+public class CalcUtil {
+
+    public Double calkresult(String stroka){
+        String[] masForCalk = stroka.split(" ");
+        int num = masForCalk.length;
+        byte[] howCalk = new byte[num]; // массив, который показывает, очередь вычислений.
+        double[] forLong = new double[num]; // массив чисел
+        for (int i = 0; i < num; i += 2 ){
+            forLong[i] = Double.valueOf(masForCalk[i]);
+        }
+        for (int i = 1; i < num; i += 2 ){
+            if (masForCalk[i].equals("*")  || masForCalk[i].equals("/")) {
+                howCalk[i] = 1;
+            }
+            else {
+                howCalk[i] = 2;
+            }
+        }
+        for (int i = 1; i < num; i +=2 ) {
+            if (howCalk[i] == 1) {
+                if (masForCalk[i].equals("*")) {
+                    forLong[i + 1] = forLong[i - 1] * forLong[i + 1];
+                    forLong[i - 1] = 0;
+                    //howCalk[i] = 0;
+                }
+                if (masForCalk[i].equals("/")) {
+                    forLong[i + 1] = forLong[i - 1] / forLong[i + 1];
+                    forLong[i - 1] = 0;
+                    //howCalk[i] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < num; i +=2 ){
+            if (howCalk[i] == 2 && masForCalk[i].equals("+")) {
+                for (int j = i; j < num; j += 2) {
+                    if (forLong[j + 1] != 0){
+                        forLong[j + 1] = forLong[i - 1] + forLong[j + 1];
+                        //forLong[i - 1] = 0;
+                        //howCalk[i] = 0;
+                        break;
+                    }
+                }
+            }
+            if (howCalk[i] == 2 && masForCalk[i].equals("-")) {
+                for (int j = i; j < num; j += 2) {
+                    if (forLong[j + 1] != 0){
+                        forLong[j + 1] = forLong[i - 1] - forLong[j + 1];
+                        //forLong[i - 1] = 0;
+                        //howCalk[i] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        return forLong[num-1];
+    }
+}
---
+++ ICalc.java
@@ -0,0 +1,6 @@
+import java.util.Queue;
+
+public interface ICalc {
+    String listCalc(String nextCalc) throws Exception;
+    Double listResultCalc(String nextResultCalc) throws Exception;
+}
